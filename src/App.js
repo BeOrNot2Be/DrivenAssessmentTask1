c904e06dc99b0dae4a14428113685bbca76352fe
BeOrNot2Be <timchick.ua@gmail.com>
2020-06-23 16:32:48 -0700

    lint fix

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 /** @format */
 
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Route, Switch } from "react-router-dom";
 import CheckerboardPage from "./screens/CheckerboardPage";
